7f6fa337cbc0c43f03b41186a74699540b4854ac
arufiand <alfiandannyarmanta@gmail.com>
2024-11-13 20:26:12 +0700

    feat: implementing bloc on login screen

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'screen/login_screen.dart';
+import 'blocs/provider.dart';
 
-class App extends StatelessWidget{
+class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  build(context){
-    return const MaterialApp(
-      title: 'Log Me In',
-      home : Scaffold(
-        body: LoginScreen(),
-      )
+  Widget build(BuildContext context) {
+    return Provider(
+      child: const MaterialApp(
+        title: 'Log Me In',
+        home: Scaffold(
+          body: LoginScreen(),
+        ),
+      ),
     );
   }
 }
